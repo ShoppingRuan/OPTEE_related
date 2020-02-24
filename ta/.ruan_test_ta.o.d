2ad01a3905c3e5769eddf8b0d641583eb3155a73
ShoppingRuan <rlq1969612634@gmail.com>
2020-02-24 23:07:34 +0800

    optee example - change from helloworld

---
+++ ta/.ruan_test_ta.o.d
@@ -0,0 +1,17 @@
+ruan_test_ta.o: ruan_test_ta.c \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/tee_internal_api.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/tee_api.h \
+ /home/ruan/op_tee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/6.2.1/include/stddef.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/compiler.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/tee_api_defines.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/tee_api_types.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/stdint.h \
+ /home/ruan/op_tee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/6.2.1/include/stdbool.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/trace.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/trace_levels.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/tee_ta_api.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/tee_internal_api_extensions.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/stdio.h \
+ /home/ruan/op_tee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/6.2.1/include/stdarg.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/tee_api_defines_extensions.h \
+ include/ruan_test_ta.h
---
+++ ta/.user_ta_header.o.d
@@ -0,0 +1,17 @@
+user_ta_header.o: \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/src/user_ta_header.c \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/compiler.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/tee_ta_api.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/tee_api_defines.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/tee_api_types.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/stdint.h \
+ /home/ruan/op_tee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/6.2.1/include/stdbool.h \
+ /home/ruan/op_tee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/6.2.1/include/stddef.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/tee_internal_api_extensions.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/trace.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/trace_levels.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/stdio.h \
+ /home/ruan/op_tee/toolchains/aarch32/lib/gcc/arm-linux-gnueabihf/6.2.1/include/stdarg.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/tee_api_defines_extensions.h \
+ /home/ruan/op_tee/optee_os/out/arm/export-ta_arm32/include/user_ta_header.h \
+ user_ta_header_defines.h include/ruan_test_ta.h
